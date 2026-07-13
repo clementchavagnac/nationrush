3631f0e8d1ac55f7519716b17de979a696e1d0fb
clementchavagnac <clement.chavagnac.24230@gmail.com>
2026-07-13 15:46:53 +1000

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import NationRush from "./NationRush.jsx";
-import "./index.css";
+* { box-sizing: border-box; margin: 0; padding: 0; }
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <NationRush />
-  </React.StrictMode>
-);
+html, body, #root {
+  height: 100%;
+  min-height: 100vh;
+}
+
+body {
+  background: #0a0e1a;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  overscroll-behavior: none;
+}
+
+/* Évite le zoom involontaire sur mobile au focus d'un champ */
+input, button, select, textarea { font-family: inherit; }
